c032dc13cf3f7aa3c098e8350a043a0ee93893da
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-07-28 19:35:10 -0400

    Update lib-default-image.rb

--- lib-default-image.rb
+++ lib-default-image.rb
@@ -1,1 +1,25 @@
 
+# frozen_string_literal: true
+
+module Faker
+  class Image < Base
+    class << self
+
+      # Images are from google , not for commercial use.
+
+      ##
+      # Produces a URL for an image
+      # 
+      # @return [String]
+      #
+      # @example
+      #   Faker::Beer.name #=> "Pliny The Elder"
+      #
+      # @faker.version 1.6.2
+      def name
+        fetch('beer.name')
+      end
+
+    end
+  end
+end
